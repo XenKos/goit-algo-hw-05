ee81ea3b67fe8704d98bff3b843817349c11ca27
XenKos <158058743+XenKos@users.noreply.github.com>
2024-02-17 20:03:07 +0100

    Update задача 4.py

--- задача 4.py
+++ задача 4.py
@@ -1,3 +1,8 @@
+def parse_input(user_input):
+    cmd, *args = user_input.split()
+    cmd = cmd.strip().lower()
+    return cmd, args  # Змінено, щоб повертати список аргументів
+
 def input_error(func):
     def inner(*args, **kwargs):
         try:
@@ -44,3 +49,29 @@ def show_all_contacts(contacts):
         return result
     else:
         return "Phonebook is empty."
+
+def main():
+    contacts = {}
+    print("Welcome to the assistant bot!")
+    while True:
+        user_input = input("Enter a command: ")
+        command, args = parse_input(user_input)  # Змінено, щоб отримати список аргументів
+
+        if command in ['exit', 'close']:
+            print("Good bye!")
+            break
+        elif command == 'hello':
+            print("How can I help you?")
+        elif command == 'add':
+            print(add_contact(args, contacts))
+        elif command == 'change':
+            print(change_contact(args, contacts))
+        elif command == 'phone':
+            print(show_phone(args, contacts))
+        elif command == 'all':
+            print(show_all_contacts(contacts))
+        else:
+            print("Invalid command. Please try again.")
+
+if __name__ == "__main__":
+    main()
